ff0ff45d349e53cfc47b9a3d2c1457043bb5eaca
Anoop Kumar Burnwal <anoopkumar19062006@gmail.com>
2025-06-12 17:06:16 +0530

    Learnt how to create custom 404 page not fund page

--- dynamic_routing/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ dynamic_routing/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,15 @@
+import { notFound } from "next/navigation";
+
+
 export default async function ReviewDetails({params} : {
     params: Promise<{productId: string, reviewId: string}>
 }){
     const productId = (await params).productId;
     const reviewId = (await params).reviewId;
+
+    if(parseInt(reviewId) > 1000){
+        notFound() ; // If reviewId is greater than 1000, show 404 page
+    }
     return (
         <h1>Review {reviewId} for Product: {productId}</h1>
     )
