3e9cebcacb64fdb458f12fe362faf2ce4adf4188
Anoop Kumar Burnwal <anoopkumar19062006@gmail.com>
2025-06-16 22:30:39 +0530

    Initial config for parallel-intercepting

---
+++ parallel-intercepting_routes/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function Home(){
+    return (
+        <>
+            <h1>Welcome to world of Routing in Next.js || Welcome Home</h1>
+            
+
+        </>
+    )
+}
